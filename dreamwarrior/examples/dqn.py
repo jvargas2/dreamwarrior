78984956b1d490e07364e756f3a385d8958aee68
Jorge Vargas <jvargas2@icloud.com>
2019-05-08 23:49:52 -0400

    Didn't commit the dqn file itself

--- dreamwarrior/examples/dqn.py
+++ dreamwarrior/examples/dqn.py
@@ -42,10 +42,37 @@ class DQN(nn.Module):
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
 
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    capacity = 0
+    memory = []
+    position = 0
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
 class DQN_Model():
-    env = None
-    # if gpu is to be used
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    env = None
+    policy_net = None
+    target_net = None
 
     def __init__(self, env):
         self.env = env
@@ -75,10 +102,10 @@ class DQN_Model():
 
         return screen.unsqueeze(0).to(self.device)
 
-    def optimize_model(self):
-        if len(memory) < BATCH_SIZE:
+    def optimize_model(self, optimizer, memory, batch_size, gamma):
+        if len(memory) < batch_size:
             return
-        transitions = memory.sample(BATCH_SIZE)
+        transitions = memory.sample(batch_size)
         # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
         # detailed explanation). This converts batch-array of Transitions
         # to Transition of batch-arrays.
@@ -87,7 +114,7 @@ class DQN_Model():
         # Compute a mask of non-final states and concatenate the batch elements
         # (a final state would've been the one after which simulation ended)
         non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=device, dtype=torch.uint8)
+                                            batch.next_state)), device=self.device, dtype=torch.uint8)
         non_final_next_states = torch.cat([s for s in batch.next_state
                                                     if s is not None])
         state_batch = torch.cat(batch.state)
@@ -97,17 +124,17 @@ class DQN_Model():
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken. These are the actions which would've been taken
         # for each batch state according to policy_net
-        state_action_values = policy_net(state_batch).gather(1, action_batch)
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
         # Compute V(s_{t+1}) for all next states.
         # Expected values of actions for non_final_next_states are computed based
         # on the "older" target_net; selecting their best reward with max(1)[0].
         # This is merged based on the mask, such that we'll have either the expected
         # state value or 0 in case the state was final.
-        next_state_values = torch.zeros(BATCH_SIZE, device=device)
-        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+        next_state_values = torch.zeros(batch_size, device=self.device)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
         # Compute the expected Q values
-        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+        expected_state_action_values = (next_state_values * gamma) + reward_batch
 
         # Compute Huber loss
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
@@ -115,7 +142,7 @@ class DQN_Model():
         # Optimize the model
         optimizer.zero_grad()
         loss.backward()
-        for param in policy_net.parameters():
+        for param in self.policy_net.parameters():
             param.grad.data.clamp_(-1, 1)
         optimizer.step()
 
@@ -141,14 +168,14 @@ class DQN_Model():
         # Get number of actions from gym action space
         n_actions = env.action_space.n
 
-        policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-        target_net = DQN(screen_height, screen_width, n_actions).to(device)
+        self.policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+        self.target_net = DQN(screen_height, screen_width, n_actions).to(device)
 
-        target_net.load_state_dict(policy_net.state_dict())
-        target_net.eval()
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
 
-        optimizer = optim.RMSprop(policy_net.parameters())
-        # memory = ReplayMemory(10000)
+        optimizer = optim.RMSprop(self.policy_net.parameters())
+        memory = ReplayMemory(10000)
 
         steps_done = 0
 
@@ -172,7 +199,7 @@ class DQN_Model():
                         # t.max(1) will return largest column value of each row.
                         # second column on max result is index of where max element was
                         # found, so we pick action with the larger expected reward.
-                        action = policy_net(state).max(1)[1].view(1, 1)
+                        action = self.policy_net(state).max(1)[1].view(1, 1)
                 else:
                     action = torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
 
@@ -181,8 +208,8 @@ class DQN_Model():
                 _, reward, done, _ = env.step(retro_action)
                 reward = torch.tensor([reward], device=device)
 
-                if t % 10:
-                    env.render()
+                # if t % 10:
+                #     env.render()
 
                 # Observe new state
                 last_screen = current_screen
@@ -193,19 +220,20 @@ class DQN_Model():
                     next_state = None
 
                 # Store the transition in memory
-                # memory.push(state, action, next_state, reward)
+                memory.push(state, action, next_state, reward)
 
                 # Move to the next state
                 state = next_state
 
                 # Perform one step of the optimization (on the target network)
-                # optimize_model()
+                self.optimize_model(optimizer, memory, batch_size, gamma)
+
                 if done:
                     episode_durations.append(t + 1)
                     break
             # Update the target network, copying all weights and biases in DQN
             if i_episode % target_update == 0:
-                target_net.load_state_dict(policy_net.state_dict())
+                self.target_net.load_state_dict(self.policy_net.state_dict())
 
             print('Finished episode loop')
 
@@ -213,42 +241,10 @@ class DQN_Model():
         print('Reached end of train function')
 
 def main():
-    env = dreamwarrior.make_custom_env('../data/', 'NightmareOnElmStreet-Nes')
+    env = dreamwarrior.make_custom_env('../data/', 'NightmareOnElmStreet-Nes', record=True)
 
     model = DQN_Model(env)
     model.train()
 
 if __name__ == '__main__':
     main()
-
-"""
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
-
-    def push(self, *args):
-        ""Saves a transition."
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = Transition(*args)
-        self.position = (self.position + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-# env.reset()
-
-
-
-
-print('Complete')
-env.render()
-env.close()"""
