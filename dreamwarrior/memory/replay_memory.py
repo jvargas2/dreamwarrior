2af5d4150ea7dea3aa293119d4c0048a54efe8ea
Jorge Vargas <jvargas2@icloud.com>
2019-07-09 21:33:01 -0400

    Move states back to GPU and change to 80x85

--- dreamwarrior/memory/replay_memory.py
+++ dreamwarrior/memory/replay_memory.py
@@ -17,22 +17,16 @@ class ReplayMemory(object):
 
     def push(self, state, action, reward, next_state, done):
         """Saves a transition."""
-        if self.device.type == 'cpu':
-            state = state.data.numpy()
-            next_state = next_state.data.numpy()
-        else:
-            state = state.data.cpu().numpy()
-            next_state = next_state.data.cpu().numpy()
         self.buffer.append((state, action, reward, next_state, done))
 
     def sample(self):
         sample = random.sample(self.buffer, self.batch_size)
         state, action, reward, next_state, done = zip(*sample)
 
-        state = torch.tensor(state, device=self.device)
+        state = torch.stack(state)
         action = torch.tensor(action, device=self.device).unsqueeze(1)
         reward = torch.tensor(reward, dtype=torch.float, device=self.device).reshape(-1, 1)
-        next_state = torch.tensor(next_state, device=self.device)
+        next_state = torch.stack(next_state)
         done = torch.tensor(done, dtype=torch.float, device=self.device).reshape(-1, 1)
 
         return state, action, reward, next_state, done
